190da108a80d5effd5534f795706d5500be97790
yunuo <xiongyy@cai-inc.com>
2016-10-27 19:22:29 +0800

    feature 调整路由，优化代码

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var config = require("./lib/config");
 
 app = express();
 
+// require express server config
 require('./lib/express')(app);
 
+// require express routers config
 require('./lib/routes')(app);
 
+// add file watcher and reload
 require('./lib/file_watcher');
 
 app.listen(config.serverPort);
